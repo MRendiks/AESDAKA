e88ffa68262251d8c06993affaa96311285d76aa
Rendaks <105725708+MRendiks@users.noreply.github.com>
2023-08-17 13:12:04 +0700

    add initial project

---
+++ aesdaka.sql
@@ -0,0 +1,102 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 26 Jan 2023 pada 17.56
+-- Versi server: 10.4.27-MariaDB
+-- Versi PHP: 7.4.33
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `aesdaka`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `file`
+--
+
+CREATE TABLE `file` (
+  `id_file` int(11) NOT NULL,
+  `username` varchar(15) DEFAULT NULL,
+  `file_name_source` varchar(255) DEFAULT NULL,
+  `file_name_finish` varchar(255) DEFAULT NULL,
+  `file_url` varchar(255) DEFAULT NULL,
+  `file_size` float DEFAULT NULL,
+  `password` varchar(16) DEFAULT NULL,
+  `tgl_upload` timestamp NULL DEFAULT NULL,
+  `status` enum('1','2') DEFAULT NULL,
+  `keterangan` varchar(255) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data untuk tabel `file`
+--
+
+INSERT INTO `file` (`id_file`, `username`, `file_name_source`, `file_name_finish`, `file_url`, `file_size`, `password`, `tgl_upload`, `status`, `keterangan`) VALUES
+(71, 'admin', '85709-alur-kas-rt-sebelum-perubahan.xlsx', '93598-alur-kas-rt-sebelum-perubahan.rda', 'file_enkripsi/93598-alur-kas-rt-sebelum-perubahan.rda', 48.457, '036d5a456c9ef2c3', '2022-06-04 02:55:17', '2', 'File Alur Kas RT');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `users`
+--
+
+CREATE TABLE `users` (
+  `username` varchar(15) NOT NULL,
+  `password` varchar(100) DEFAULT NULL,
+  `fullname` varchar(50) DEFAULT NULL,
+  `job_title` varchar(50) DEFAULT NULL,
+  `join_date` timestamp NULL DEFAULT NULL,
+  `last_activity` timestamp NULL DEFAULT NULL,
+  `status` enum('1','2') DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data untuk tabel `users`
+--
+
+INSERT INTO `users` (`username`, `password`, `fullname`, `job_title`, `join_date`, `last_activity`, `status`) VALUES
+('admin', '21232f297a57a5a743894a0e4a801fc3', 'Administrator', 'Project Manager', '2017-04-28 08:48:55', '2022-06-08 05:36:27', '1');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `file`
+--
+ALTER TABLE `file`
+  ADD PRIMARY KEY (`id_file`);
+
+--
+-- Indeks untuk tabel `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`username`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `file`
+--
+ALTER TABLE `file`
+  MODIFY `id_file` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=75;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
